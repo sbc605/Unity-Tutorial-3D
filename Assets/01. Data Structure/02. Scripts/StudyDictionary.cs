e15523735d60cad0d9f33244fa423a031b77cf6d
sbc605 <sbc605@naver.com>
2025-07-08 14:46:05 +0900

    Dictionary 스크립트 수정

--- Assets/01. Data Structure/02. Scripts/StudyDictionary.cs
+++ Assets/01. Data Structure/02. Scripts/StudyDictionary.cs
@@ -1,42 +1,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PersonData
+{
+    public int age;
+    public string name;
+    public float height;
+    public float weight;
+
+    public PersonData(int age, string name, float height, float weight)
+    {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public PersonData(int age, float height, float weight) // 오버로드
+    {
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+}
+
 public class StudyDictionary : MonoBehaviour
 {
-    public Dictionary<string, int> persons = new Dictionary<string, int>();
+    public Dictionary<string, PersonData> persons = new Dictionary<string, PersonData>();
 
     void Start()
     {
-        // Dictionary에 데이터 추가
-        persons.Add("철수", 10);
-        persons.Add("영희", 15);
-        persons.Add("동수", 17);
+        persons.Add("철수", new PersonData(10, "철수", 150f, 30f));
+        persons.Add("영희", new PersonData(10, 150f, 30f));
+        persons.Add("동수", new PersonData(10, "동수", 150f, 30f));
 
-        persons["철수"] = 13; // 철수 값 덮어쓰기
-
-        int age = persons["철수"]; // Key 값으로 Value를 출력
-        Debug.Log($"철수의 나이는 {age}입니다.");
-
-
-        // value를 조건문으로 Key값 찾기
-        foreach (var person in persons)
-        {
-            if (person.Value == 15)
-            {
-                Debug.Log($"나이가 15인 사람의 이름은 {person.Key}입니다.");
-            }
-
-            Debug.Log($"{person.Key}의 나이는 {person.Value}입니다.");
-        }
-
-        if (persons.ContainsKey("철수"))
-        {
-            Debug.Log("사람 중에 철수가 있음");
-        }
-
-        if (persons.ContainsValue(17))
-        {
-            Debug.Log("17살인 사람이 있다.");
-        } 
-    }
+        Debug.Log(persons["철수"].age);
+        Debug.Log(persons["철수"].name);
+        Debug.Log(persons["철수"].height);
+        Debug.Log(persons["철수"].weight);
+    }    
 }
