5afb04651bc0c6279cbc5a6ed294e0a1370c7dcb
sbc605 <sbc605@naver.com>
2025-07-09 11:15:10 +0900

    Dev Scripts

---
+++ Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public int number = 100;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
