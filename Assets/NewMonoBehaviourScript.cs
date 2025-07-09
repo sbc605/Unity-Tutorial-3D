1425e8d5e6866ee2b3ea66924232ee2679a2ef63
sbc605 <sbc605@naver.com>
2025-07-09 11:28:24 +0900

    Main Merge EDIT

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -2,11 +2,7 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-<<<<<<< HEAD
     public int number = 10;
-=======
-    public int number = 100;
->>>>>>> Dev
 
     void Start()
     {
